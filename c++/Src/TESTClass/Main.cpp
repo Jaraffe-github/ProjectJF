22cae3a593fa7092906c22c3cb2915b0a714cd3e
SeungMinChoi <ccm1333@naver.com>
2016-08-21 23:44:02 +0900

    해야할것이 넘친다!!!

--- c++/Src/TESTClass/Main.cpp
+++ c++/Src/TESTClass/Main.cpp
@@ -4,6 +4,9 @@
 // 문자열 클래스.
 #include "JFStringClass/JFStringClasses.h"
 
+// 공부용 클래스.
+#include "JFStudyClasses/JFStudyClasses.h"
+
 // test
 #include <iostream>
 
@@ -17,9 +20,33 @@ void main()
 #endif
 
 	// String Reverse
-	char Test[] = "I Don't have any Question";
-	std::cout << Test << std::endl;
-	JF::JFStringUT::Reverse(Test);
-	std::cout << Test << std::endl;
+	{
+		char Test[] = "I Don't have any Question";
+		std::cout << Test << std::endl;
+		JF::JFStringUT::Reverse(Test);
+		std::cout << "Reverse" << Test << std::endl;
+		std::cout << std::endl;
+	}
+	
+	// DDL Node
+	{
+		JF::JFStudy::JFLinkedList<int> NodeTest;
+		NodeTest.Push_Back(1);
+		NodeTest.Push_Back(6);
+		NodeTest.Push_Back(8);
+		NodeTest.Push_Back(4);
+		NodeTest.Push_Back(2);
 
+		std::cout << "Node Count : " << NodeTest.GetCount() << std::endl;
+
+		std::cout << "Node Datas" << std::endl;
+		auto iterNode = NodeTest.BeginNode();
+		while (iterNode != nullptr)
+		{
+			std::cout << iterNode->pData << " ";
+			iterNode = iterNode->pNextNode;
+		}
+		std::cout << std::endl;
+		std::cout << std::endl;
+	}
 }
--- c++/include/JFStudyClasses/JFLinkedList.h
+++ c++/include/JFStudyClasses/JFLinkedList.h
@@ -16,68 +16,117 @@ namespace JF
 		class JFLinkedList
 		{
 		private:
-			DLLNode* CreateNode(TData p_Data);
-			void Destroy(DLLNode* p_Node);
+			DLLNode<TData>* CreateNode(TData p_Data);
+			void Destroy(DLLNode<TData>* p_Node);
 
 		public:
+			void Push_Front(TData p_Data);
 			void Push_Back(TData p_Data);
-			void Remove(DLLNode* p_Node);
+			void Remove(DLLNode<TData>* p_Node);
+
+			int GetCount();
+			DLLNode<TData>* BeginNode();
 
 		private:
-			DLLNode* m_pHead = nullptr;
-			DLLNode* m_pTail = nullptr;
+			DLLNode<TData>* m_pHead = nullptr;
+			DLLNode<TData>* m_pTail = nullptr;
 		};
+
+		class Iterator
+		{
+			// 이후 심심할때 구현해보자.
+		};
+
+		template<typename TData>
+		DLLNode<TData>* JF::JFStudy::JFLinkedList<TData>::CreateNode(TData p_Data)
+		{
+			DLLNode<TData>* newNode = (DLLNode<TData>*)malloc(sizeof(DLLNode<TData>));
+			newNode->pData = p_Data;
+			newNode->pNextNode = nullptr;
+			newNode->pPrevNode = nullptr;
+
+			return newNode;
+		};
+
+		template<typename TData>
+		void JF::JFStudy::JFLinkedList<TData>::Destroy(DLLNode<TData>* p_Node)
+		{
+			free(p_Node);
+		}
+
+		template<typename TData>
+		void JF::JFStudy::JFLinkedList<TData>::Push_Front(TData p_Data)
+		{
+			if (m_pHead == nullptr && m_pTail == nullptr)
+			{
+				m_pHead = CreateNode(p_Data);
+				m_pTail = m_pHead;
+			}
+			else
+			{
+				if (m_pHead->pNextNode != nullptr)
+					m_pHead->pNextNode = CreateNode(p_Data);
+
+				if (m_pHead == m_pTail)
+					m_pTail = m_pHead->pNextNode;
+			}
+		}
+
+		template<typename TData>
+		void JF::JFStudy::JFLinkedList<TData>::Push_Back(TData p_Data)
+		{
+			if (m_pHead == nullptr && m_pTail == nullptr)
+			{
+				m_pHead = CreateNode(p_Data);
+				m_pTail = m_pHead;
+			}
+			else
+			{
+				m_pTail->pNextNode = CreateNode(p_Data);
+				m_pTail->pNextNode->pPrevNode = m_pTail;
+				m_pTail = m_pTail->pNextNode;
+			}
+		}
+
+		template<typename TData>
+		void JF::JFStudy::JFLinkedList<TData>::Remove(DLLNode<TData>* p_Node)
+		{
+			DLLNode<TData>* prevNode = p_Node->pPrevNode;
+			DLLNode<TData>* nextNode = p_Node->pNextNode;
+
+			if (prevNode == nullptr)
+				m_pHead = nextNode;
+			else
+				prevNode->pNextNode = nextNode;
+
+			if (nextNode == nullptr)
+				m_pTail = prevNode;
+			else
+				nextNode->pPrevNode = prevNode;
+
+			Destroy(p_Node);
+		}
+
+		template<typename TData>
+		int JF::JFStudy::JFLinkedList<TData>::GetCount()
+		{
+			int nCount = 0;
+
+			auto iterBegin = m_pHead;
+			while (iterBegin != nullptr)
+			{
+				iterBegin = iterBegin->pNextNode;
+				++nCount;
+			}
+
+			return nCount;
+		}
+
+		template<typename TData>
+		DLLNode<TData>* JF::JFStudy::JFLinkedList<TData>::BeginNode()
+		{
+			return m_pHead;
+		}
 	}
 }
 
-template<typename TData>
-DLLNode<TData>* JF::JFStudy::JFLinkedList<TData>::CreateNode(TData p_Data)
-{
-	DLLNode<TData>* newNode = (DLLNode<TData>*)malloc(sizeof(DLLNode<T>));
-	newNode->pData		= p_Data;
-	newNode->pNextNode	= nullptr;
-
-	return newNode;
-};
-
-template<typename TData>
-void JF::JFStudy::JFLinkedList<TData>::Destroy(DLLNode* p_Node)
-{
-	free(p_Data);
-}
-
-template<typename TData>
-void JF::JFStudy::JFLinkedList<TData>::Push_Back(TData p_Data)
-{
-	if (m_pHead == nullptr && m_pTail == nullptr)
-	{
-		m_pHead = CreateNode(p_Data);
-		m_pTail = m_pHead;
-	}
-	else
-	{
-		m_pTail->pNextNode = CreateNode(p_Data);
-		m_pTail->pNextNode->pPrevNode = m_pTail;
-		m_pTail = m_pTail->pNextNode;
-	}
-}
-
-
-template<typename TData>
-void JF::JFStudy::JFLinkedList<TData>::Remove(DLLNode* p_Node)
-{
-	auto prevNode = p_Data->pPrevNode;
-	auto nextNode = p_Data->pNextNode;
-
-	if (prevNode == nullptr)
-		m_pHead = nextNode;
-	else
-		prevNode->pNextNode = nextNode;
-
-	if (nextNode == nullptr)
-		m_pTail = prevNode;
-	else
-		nextNode->pPrevNode = prevNode;
-
-	Destroy(p_Node);
-}
