40708852f020f10d84d08576da80eb8835e0c83f
SeungMinChoi <ccm1333@naver.com>
2016-10-03 21:08:43 +0900

    Add Build .Bat File And ProjectSetting Modify

---
+++ c++/Src/buildJF.vs2015.x64.bat
@@ -0,0 +1,28 @@
+@ECHO ------------------------------------------
+@ECHO            JFProject for C++
+@ECHO             Build Projects
+@ECHO:
+@ECHO      	x64 Debug/Release
+@ECHO        Build for Visual Studio 2015
+@ECHO ------------------------------------------
+@ECHO @ Start Compile
+
+@ECHO OFF
+call "%VS140COMNTOOLS%vsvars32.bat"
+
+
+:COMPILE_COMMON_CLASSES
+IF EXIST "JFCommonClasses.vs2015.sln" GOTO COMPILE_3D_CLASSES
+  @ECHO @ Compile JFCommon Classes
+  devenv JFCommonClasses.sln /rebuild "Debug|x64"
+  devenv JFCommonClasses.sln /rebuild "Debug_Dynamic|x64"
+  devenv JFCommonClasses.sln /rebuild "Release|x64"
+  devenv JFCommonClasses.sln /rebuild "Release_Dynamic|x64"
+
+:COMPILE_COMPLETE
+@ECHO ------------------------------------------
+@ECHO @ Complete!!! (For Visual Studio 2015)
+@ECHO          x64 Debug/Release
+@ECHO ------------------------------------------
+
+pause
